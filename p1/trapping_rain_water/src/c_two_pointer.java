a9c833c21b5f451be1837f6edde6dfe316f1b7c9
salazangar <sidharthm673@gmail.com>
2024-11-19 18:41:24 +0530

    adding 2 pointer method

--- p1/trapping_rain_water/src/c_two_pointer.java
+++ p1/trapping_rain_water/src/c_two_pointer.java
@@ -7,11 +7,24 @@ public class c_two_pointer {
 
     private static int maxWater(int[] arr) {
         int n =arr.length;
-        int left = n-2;
-        int right = n-1;
-        while(left != 0){
+        int left = 1;
+        int right = n-2;
+        int lmax = arr[0];
+        int rmax = arr[n-1];
+        int res = 0;
 
+        while(left <= right){
+
+            if(lmax >= rmax){               // move l towards right
+                res += Math.max(0, rmax - arr[right]);
+                rmax = Math.max(rmax, arr[right]);
+                right--;
+            }else{
+                res += Math.max(0, lmax - arr[left]);
+                lmax = Math.max(lmax, arr[left]);
+                left++;
+            }
         }
-        return 0;
+        return res;
     }
 }
