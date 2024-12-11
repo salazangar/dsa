e2b78ce393d6e343e24e63b837e5d3fc9dfacb44
salazangar <sidharthm673@gmail.com>
2024-12-11 10:13:32 +0530

    new methods

--- p1/Find Smallest Missing Positive Number/src/Main.java
+++ p1/Find Smallest Missing Positive Number/src/Main.java
@@ -7,7 +7,9 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {2, -3, 4, 1, 1, 7};
 //        System.out.println(findMissingNaive(arr));
-        System.out.println(findMissingVisitedArray(arr));
+//        System.out.println(findMissingVisitedArray(arr));
+//        System.out.println(findMissingMarkingIndices(arr));
+        System.out.println(findMissingNegating(arr));
     }
 
     public static int findMissingNaive(int[] arr){
@@ -39,4 +41,91 @@ public class Main {
         }
         return n+1;
     }
+
+    // mark all indices outside range (n) and negative numbers as 1
+    public static int findMissingMarkingIndices(int[] arr){
+        int n = arr.length;
+        boolean flag = false;
+
+        for (int i = 0; i < n; i++) {
+            if(arr[i] == 1){
+                flag = true;
+                break;
+            }
+        }
+        if(!flag){
+            return 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if(arr[i] <= 0 || arr[i] > n){
+                arr[i] = 1;
+            }
+        }
+
+        // mark all occurences in range as ">n"
+        for (int i = 0; i < n; i++) {
+            if(arr[i] <= n){
+                arr[(arr[i]-1) % n] += n;   // "%n" because if already marked, to remove outOfBoundErrors
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if(arr[i] <= n){
+                return i+1;
+            }
+        }
+        // all numbers from 1 to n are present
+        return n+1;
+    }
+
+    public static int findMissingNegating(int[] arr){
+        int n = arr.length;
+        int k = partition(arr);
+
+        for (int i = 0; i < k; i++) {
+            int val = Math.abs(arr[i]);  // abs() in case of already marked negative
+
+            if(val-1 < k && arr[val - 1] > 0){
+                arr[val-1] = -arr[val-1];
+            }
+        }
+        for (int i = 0; i < k; i++){
+            if(arr[i] > 0){
+                return i+1;
+            }
+        }
+        return k+1;
+    }
+
+    public static int partition(int[] arr){
+        int n = arr.length;
+        int pivot = 0;
+
+        for (int i = 0; i < n; i++) {
+            if(arr[i] > arr[pivot]){
+                int temp = arr[i];
+                arr[i] = arr[pivot];
+                arr[pivot] = arr[i];
+                pivot++;
+            }
+        }
+        return pivot;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
