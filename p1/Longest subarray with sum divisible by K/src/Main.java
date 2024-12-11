0db08fb401a96183215f47c5c9a606bad7af9e3e
salazangar <sidharthm673@gmail.com>
2024-12-11 12:25:46 +0530

    map

--- p1/Longest subarray with sum divisible by K/src/Main.java
+++ p1/Longest subarray with sum divisible by K/src/Main.java
@@ -2,6 +2,9 @@
 // The problem is to find the longest sub-array's length with the sum of the elements
 // divisible by the given value k.
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         int[] arr = {2, 7, 6, 1, 4, 5};
@@ -28,6 +31,26 @@ public class Main {
     public static int maxLengthSliding(int[] arr, int k){
         int n = arr.length;
 
-        
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] rem_arr = new int[n];
+        int sum = 0;
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+
+            rem_arr[i] = ((sum %k) + k) % k;   // accounting for negative sum value also
+
+            if(rem_arr[i] == 0){
+                max = i + 1;
+            } else if (!map.containsKey(rem_arr[i])) {
+                map.put(rem_arr[i], i);
+            }else{
+                if(max < i - map.get(rem_arr[i])){
+                    max = i - map.get(rem_arr[i]);
+                }
+            }
+        }
+        return max;
     }
 }
