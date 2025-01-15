7771f5eaa3c5b39d2439441c0a038363f50a7be9
salazangar <sidharthm673@gmail.com>
2025-01-15 19:40:24 +0530

    left view of tree

--- p2/Left View of a Binary Tree/src/Main.java
+++ p2/Left View of a Binary Tree/src/Main.java
@@ -1,7 +1,5 @@
 import java.net.Inet4Address;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 class Node{
     int data;
@@ -22,12 +20,58 @@ public class Main {
         root.left.left = new Node(4);
         root.left.right = new Node(5);
 
-        ArrayList<Integer> result = leftView(root);
+//        ArrayList<Integer> result = leftViewRecursion(root);
+        ArrayList<Integer> result = leftViewLevelOrder(root);
 
         System.out.println(result);
     }
 
-    public static ArrayList<Integer> leftView(Node root){
+    public static ArrayList<Integer> leftViewRecursion(Node root){
+        int[] level = new int[]{0};
+        ArrayList<Integer> res = new ArrayList<>();
+        recursion(root,0, level, res);
+        return res;
+    }
+    public static void recursion(Node node,int level, int[] maxLevel, ArrayList<Integer> res){
+        if(node == null) return;
 
+        if(maxLevel[0] <= level){
+            res.add(node.data);
+            maxLevel[0] = level + 1;
+        }
+
+        recursion(node.left, level + 1, maxLevel, res);
+        recursion(node.right, level + 1, maxLevel, res);
+    }
+
+    public static ArrayList<Integer> leftViewLevelOrder(Node root){
+        if(root == null) return new ArrayList<>();
+
+        Queue<Node> q = new LinkedList<>();
+        ArrayList<Integer> res = new ArrayList<>();
+
+        q.add(root);
+        while(!q.isEmpty()){
+            int len = q.size();
+
+            for (int i = 0; i < len; i++) {
+                Node curr = q.poll();
+                if(i == 0) res.add(curr.data);
+
+                if(curr.left != null) q.add(curr.left);
+                if(curr.right != null) q.add(curr.right);
+            }
+        }
+        return res;
     }
 }
+
+
+
+
+
+
+
+
+
+
